3866427f54dcac249a31e722137c58f55755f34a
shinyeon57 <noeys57@sookmyung.ac.kr>
2020-10-29 15:30:35 +0900

    practice 02

--- main.c
+++ main.c
@@ -6,14 +6,23 @@
 int main(int argc, char *argv[]) {
 	
 	int	i;
+	int average;
+	int sum = 0;
 	int grade[SIZE];
+
+
 	printf("input the grade of 5 students.\n ");
-	
+
 	for(i=0;i<SIZE;i++)
 		scanf("%d", &grade[i]);
-		
+
 	for(i=0;i<SIZE;i++)
+		{
 		printf("grade[%d] = %d\n", i, grade[i]);
-			
+		sum += grade[i];
+	}
+	average = sum/SIZE;
+	printf("average of grade : %d\n", average);		
+
 	return 0;
 }
